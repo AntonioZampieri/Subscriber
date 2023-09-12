4fcf0046e9f115d3a9ccb2e6cb5dae9531c5bb5d
AntonioZampieri <Antonio.zampieri.1996@gmail.com>
2023-09-13 01:43:37 +0200

    Add project files.

---
+++ Subscriber/Program.cs
@@ -0,0 +1,14 @@
+﻿static void subscribe(string subscriberName)
+{
+    throw new NotImplementedException();
+}
+
+static void publish(string messageText)
+{
+    throw new NotImplementedException();
+}
+
+static List<string> getMessages(string subscriberName)
+{
+    throw new NotImplementedException();
+}
